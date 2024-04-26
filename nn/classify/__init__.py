8e3bd3c61b5685282ea7d5982a5ce6026ee4912d
Ashu Jha <ashujha4444@gmail.com>
2024-04-26 17:36:26 +0530

    Add files via upload

---
+++ nn/classify/__init__.py
@@ -0,0 +1,6 @@
+
+from ultralytics.models.yolo.classify.predict import ClassificationPredictor
+from ultralytics.models.yolo.classify.train import ClassificationTrainer
+from ultralytics.models.yolo.classify.val import ClassificationValidator
+
+__all__ = "ClassificationPredictor", "ClassificationTrainer", "ClassificationValidator"
