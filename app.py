2f9c71606ba9d005f75e0cd82e5344b9ddab0d66
Ashu Jha <ashujha4444@gmail.com>
2024-04-26 13:00:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, Response, redirect, url_for
-from BlindAssistantWeb import BlindAssistant
+from flask import Flask, render_template, Response, redirect, url_for,jsonify
+from blindenv import BlindAssistant
 import sys
 
 app = Flask(__name__)
 blind_assistant = None
+analysis_text = ""  
 
 @app.route('/')
 def index():
@@ -28,9 +29,17 @@ def end_program():
 def assistant():
     return render_template('assistant.html')
 
+@app.route('/get_analysis_text')
+def get_analysis_text():
+    global blind_assistant
+    if blind_assistant is not None:
+        analysis_text = blind_assistant.analysis_text
+        return f'Analysis Text={analysis_text}'
+    else:
+        return 'Analysis Text=Blind Assistant is not running'
+
 @app.route('/video_feed')
 def video_feed():
-    global blind_assistant
     if blind_assistant is not None:
         return Response(blind_assistant.generate_frames_with_audio(), mimetype='multipart/x-mixed-replace; boundary=frame')
     else:
